ab6d40e02591ae8859748131b7bdd1ca68c42463
Patrick Roche <patrick.l.roche@gmail.com>
2016-01-14 07:30:23 -0500

    Create Cat function and move cat logic to outside of ViewModel

--- js/app.js
+++ js/app.js
@@ -1,29 +1,41 @@
-var ViewModel = function() {
+var Cat = function() {
+
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 	this.imgAttribution = ko.observable('https://github.com/udacity/ud989-cat-clicker-ko-starter/tree/master/img');
-	this.level = ko.observable('noob');
 	this.nicknames = ko.observableArray([
 		"Baby Dupree",
 		"Eater of Worlds",
 		"Lil' Hellspawn"]);
 
-	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
+	this.level = ko.computed(function() {
+		var level;
+		var clicks = this.clickCount();
 
-
-
-		if(this.clickCount() < 5) {
-				this.level("rookie");
-		} else if(this.clickCount() < 10) {
-				this.level("regular");
-		} else if(this.clickCount() < 15) {
-				this.level("veteran");
+		if (clicks < 5) {
+			level = 'Noob';
+		} else if (clicks < 10) {
+			level = "Rookie";
+		} else if(clicks < 15) {
+			level = "Normal";
+		} else if(clicks < 20) {
+			level = "Veteran";
 		} else {
-				this.level("o.g.");
+			level = "O.G.";
 		}
+
+		return level;
+	}, this);
+};
+
+var ViewModel = function() {
+
+	this.currentCat = ko.observable(new Cat());
+
+	this.incrementCounter = function() {
+		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
 	};
-}
+};
 
 ko.applyBindings(new ViewModel());
