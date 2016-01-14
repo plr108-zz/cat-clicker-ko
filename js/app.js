f0426ddfc1255efb9f94125fcaa17d07346db0d4
Patrick Roche <patrick.l.roche@gmail.com>
2016-01-14 09:35:58 -0500

    Refactor to select a cats from a list

--- js/app.js
+++ js/app.js
@@ -1,28 +1,57 @@
-var Cat = function() {
+var initialCats = [
+	{
+		clickCount : 0,
+		name : 'Baby Dupree',
+		imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+		imgAttribution : 'https://github.com/udacity/ud989-cat-clicker-ko-starter/tree/master/img',
+		nicknames : ["Murrysville Mauler", "Eater of Worlds", "Lil' Hellspawn"]
+	},
 
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.imgAttribution = ko.observable('https://github.com/udacity/ud989-cat-clicker-ko-starter/tree/master/img');
-	this.nicknames = ko.observableArray([
-		"Baby Dupree",
-		"Eater of Worlds",
-		"Lil' Hellspawn"]);
+	{
+		clickCount : 0,
+		name : 'Baby Duke',
+		imgSrc : 'img/9648464288_2516b35537_z.jpg',
+		imgAttribution : 'https://github.com/udacity/ud989-cat-clicker-ko-starter/tree/master/img',
+		nicknames : ["Lap Cat", "Silent But Deadly", "Claws"]
+	}
+];
+
+var Cat = function(data) {
+
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgAttribution);
+	this.nicknames = ko.observable(data.nicknames);
 
 	this.level = ko.computed(function() {
 		var level;
 		var clicks = this.clickCount();
 
 		if (clicks < 5) {
-			level = 'Noob';
+			level = "Meh";
 		} else if (clicks < 10) {
-			level = "Rookie";
+			level = "Okay";
 		} else if(clicks < 15) {
-			level = "Normal";
+			level = "Cool";
 		} else if(clicks < 20) {
-			level = "Veteran";
-		} else {
-			level = "O.G.";
+			level = "Rad";
+		} else if(clicks < 30) {
+			level = "Awesome";
+		} else if(clicks < 50) {
+			level = "Freaking Awesome";
+		} else if(clicks < 70) {
+			level = "Really Freaking Awesome";
+		} else if(clicks < 100) {
+			level = "You're Still Clicking??";
+		}
+		else {
+			level = "You must really "
+			for(i = 0; i < clicks - 100; i++)
+			{
+				level += " really "
+			}
+			level += " love this picture";
 		}
 
 		return level;
@@ -31,11 +60,25 @@ var Cat = function() {
 
 var ViewModel = function() {
 
-	this.currentCat = ko.observable(new Cat());
+	// self is the ViewModel binding
+	var self = this;
+
+	// self.catList
+	this.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(catItem) {
+		self.catList.push(new Cat(catItem));
+	});
+
+	this.currentCat = ko.observable(this.catList()[0]);
 
 	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
 	};
+
+	this.setCat = function(clickedCat) {
+		self.currentCat(clickedCat);
+	}
 };
 
 ko.applyBindings(new ViewModel());
